33d0ee506ed8a4cc061fa01f8971dc86f3ee0289
wuzhixiang <wuzhixiang@xiaobing.ai>
2022-07-07 16:22:54 +0800

    feat: 完善eslint 代码格式化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
-  extends: ["react-app", "react-app/jest"],
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      jsx: true,
-    },
-    ecmaVersion: 7,
-    sourceType: "module",
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier"],
   settings: {
-    version: "detect",
-    createClass: "createReactClass",
-    pragma: "React",
-    flowVersion: "0.53",
-    "import/resolver": {
+    version: 'detect',
+    createClass: 'createReactClass',
+    pragma: 'React',
+    flowVersion: '0.53',
+    'import/resolver': {
       node: {
-        moduleDirectory: ["node_modules", "src"],
+        moduleDirectory: ['node_modules', 'src'],
       },
     },
   },
@@ -29,49 +24,48 @@ module.exports = {
   },
   rules: {
     // 关闭未使用变量校验
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    'no-unused-vars': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     // 禁止var
-    "no-var": 2,
+    'no-var': 2,
     // 禁止with
-    "no-with": 2,
+    'no-with': 2,
     // 禁止new Function
-    "no-new-func": 2,
+    'no-new-func': 2,
     // 禁止new Object
-    "no-new-object": 2,
+    'no-new-object': 2,
     // 禁止new基本数据类型的包装类
-    "no-new-wrappers": 2,
+    'no-new-wrappers': 2,
     // 禁止在内置对象上进行扩展（如猴子补丁）
-    "no-extend-native": 2,
+    'no-extend-native': 2,
     // 禁止直接抛出字面量
-    "no-throw-literal": 2,
+    'no-throw-literal': 2,
     // new的时候必须加上括号
-    "new-parens": 2,
+    'new-parens': 2,
     // switch必须加上default处理
-    "default-case": 2,
+    'default-case': 2,
     // 每个变量需要分别声明
-    "one-var": [
+    'one-var': [
       2,
       {
-        var: "never",
-        let: "never",
-        const: "never",
+        var: 'never',
+        let: 'never',
+        const: 'never',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
       },
     ],
     // 能用const就const，保存文件时会自动校验let变量如果没有赋值语句就自动转为const，并且强制解构获取的数据为const类型
-    "prefer-const": [1, { destructuring: "all" }],
+    'prefer-const': [1, { destructuring: 'all' }],
     // 除了null比较以外一律使用===
-    eqeqeq: ["error", "always", { null: "ignore" }],
-    "react-hooks/exhaustive-deps": "off",
-    "no-extend-native": "off",
-    "prettier/prettier": [
-      "warn",
-      {
-        singleQuote: true,
-        endOfLine: "auto",
-        printWidth: 120,
-      },
-    ],
-    "arrow-body-style": ["warn", "as-needed"],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+    'react-hooks/exhaustive-deps': 'off',
+    'no-extend-native': 'off',
+    'arrow-body-style': ['warn', 'as-needed'],
   },
 };
