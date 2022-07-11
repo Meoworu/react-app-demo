686762fce1166962b8b65f8f8203b682de469279
wuzhixiang <wuzhixiang@xiaobing.ai>
2022-07-11 14:11:14 +0800

    feat: 完善接入虚拟人demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
+import { Main } from '@/pages/Main';
+import { Layout } from '@/components/Layout';
 import '@/assets/less/global.less';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="*" element={<App />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="*" element={<Main />} />
+      </Routes>
+    </Layout>
   </BrowserRouter>
 );
