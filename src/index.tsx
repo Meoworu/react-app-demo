2c13784682f8ab78df62bda6f2380485c363789f
wuzhixiang <wuzhixiang@xiaobing.ai>
2022-07-11 10:26:03 +0800

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
-import '@/assets/less/global.less';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
+import '@/assets/less/global.less';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
