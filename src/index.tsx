d33c5a5a1e2bf4ac0d6d6db2320a5422c4e138a1
wuzhixiang <wuzhixiang@xiaobing.ai>
2022-07-11 10:24:16 +0800

    feat: 增加router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/assets/less/global.less';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
